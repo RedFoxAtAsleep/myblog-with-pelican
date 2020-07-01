b542032cb544f134d05bc577637ddcf333245db7
zhaojinhui <bbxxone@qq.com>
2020-07-01 12:16:08 +0800

    2020.7.1

--- deploycontent.sh
+++ deploycontent.sh
@@ -1,4 +1,4 @@
-conda activate pelican  # 激活环境
+source venv/bin/activate  # 激活环境
 pelican content  # pelican命令 content文件夹路径
 ghp-import output -b gh-pages  # output文件夹路径 gh-pages分支
 git push origin gh-pages
